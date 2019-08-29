3b640d6f5ec558041884b9c3bbc73ed172e26093
BeckerP <54329289+PamsterHamster@users.noreply.github.com>
2019-08-29 13:30:21 -0500

    Add files via upload

---
+++ lamb/lamb.py
@@ -0,0 +1,12 @@
+#  variables  (Ingredients)
+name = "John"
+adjective = "massive"
+animal = "dragon"
+color = "magenta"
+noun = "coffee"
+
+#  program Logic  (steps)
+print(name + " had a " + adjective + " " + animal)
+print( " Whose fleece was " + color + " " + noun)
+print( " Everywhere that " + name + " " + " went ")
+print( " the " + animal + " " + " was sure to go ")
